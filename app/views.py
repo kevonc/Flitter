a6bc4238f5218f7ed18888b812fbe16f36f4a880
Kevon Cheung <kevon.cheung@gmail.com>
2013-10-04 11:01:22 +0800

    Initialized app, set up basic routes and templates

---
+++ app/views.py
@@ -0,0 +1,27 @@
+from flask import render_template
+from app import app
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return redirect(url_for('show_posts'))
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    return redirect(url_for('show_posts'))
+
+@app.route('/flitter')
+def show_posts():
+
+    return render_template('show_posts.html', title = 'Home', user=user, posts=posts)
+
+@app.route('/flitter/new')
+def new_post():
+    return render_template('new_post.html')
+
+@app.route('/user/<username>')
+def user_posts(username=None):
+    return render_template('show_posts.html', title = 'Home', user=username)
