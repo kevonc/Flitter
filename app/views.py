4c013c48fe5622a1813b1e070fde2bb7ce751c9e
Kevon Cheung <kevon.cheung@gmail.com>
2013-10-04 11:22:40 +0800

    Fixed create new post btn url, added new post form, added post method to new_post

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, redirect
 from app import app
 
 @app.route('/')
@@ -15,10 +15,20 @@ def signup():
 
 @app.route('/flitter')
 def show_posts():
-
+    user = { 'nickname': 'Miguel' }
+    posts = [
+        {
+            'author': { 'nickname': 'John' },
+            'body': 'Beautiful day in Seattle!'
+        },
+        {
+            'author': { 'nickname': 'Susan' },
+            'body': 'Beautiful day in Boston!'
+        }
+    ]
     return render_template('show_posts.html', title = 'Home', user=user, posts=posts)
 
-@app.route('/flitter/new')
+@app.route('/flitter/new', methods=['GET', 'POST'])
 def new_post():
     return render_template('new_post.html')
 
